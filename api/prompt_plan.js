cf7211d325fbdd430ba001fdd5c9744f2daaf2ca
tri-naranjus <36755608+tri-naranjus@users.noreply.github.com>
2025-07-27 19:52:12 +0200

    Update prompt_plan.js

--- api/prompt_plan.js
+++ api/prompt_plan.js
@@ -1,8 +1,6 @@
 
 const prompt = `
 
-Actúa como **nutricionista experto en fisiología hormonal y crononutrición deportiva**.
-
 Diseña plan nutricional diario (3 comidas principales + pre/post entreno)
 Se te pasan todo los datos del usuario utiliza el GET para calcular el menu diario
 utiliza la info que tienes en memoria
